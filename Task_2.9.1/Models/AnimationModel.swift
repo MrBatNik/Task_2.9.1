922000b06756954e5232411d43212d5fbfdc5e90
MrBatNik <nicandreo@gmail.com>
2021-11-24 14:39:30 +0300

    Add animation

--- Task_2.9.1/Models/AnimationModel.swift
+++ Task_2.9.1/Models/AnimationModel.swift
@@ -5,4 +5,26 @@
 //  Created by Никита Бат on 24.11.2021.
 //
 
-import Foundation
+import Spring
+
+struct AnimationModel {
+    
+    let preset: Spring.AnimationPreset
+    let curve: Spring.AnimationCurve
+    let force: Double
+    let duration: Double
+    let delay: Double
+    
+}
+
+extension AnimationModel {
+    
+    static func setAnimationSettings() -> AnimationModel {
+        AnimationModel(preset: Spring.AnimationPreset.allCases.randomElement() ?? .zoomOut,
+                       curve: Spring.AnimationCurve.allCases.randomElement() ?? .easeIn,
+                       force: Double.random(in: 1.0...2.0),
+                       duration: Double.random(in: 1.0...2.0),
+                       delay: Double.random(in: 0.0...1.0))
+    }
+    
+}
--- Task_2.9.1/ViewControllers/AnimationViewController.swift
+++ Task_2.9.1/ViewControllers/AnimationViewController.swift
@@ -5,13 +5,45 @@
 //  Created by Никита Бат on 24.11.2021.
 //
 
-import UIKit
+import Spring
 
-class AnimationViewController: UIViewController {
+final class AnimationViewController: UIViewController {
 
+    @IBOutlet var animatedView: SpringView!
+    @IBOutlet var runButton: UIButton!
+    @IBOutlet var settingsLabels: [UILabel]!
+    
+    var animationSettings = AnimationModel.setAnimationSettings()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setLabelsTitles()
     }
+    
+    @IBAction func tapRunButton() {
+        animatedView.animation = "\(animationSettings.preset)"
+        animatedView.animate()
+        
+        setLabelsTitles()
+        animationSettings = AnimationModel.setAnimationSettings()
+        runButton.setTitle("\(animationSettings.preset)", for: .normal)
+    }
+    
+}
 
+private extension AnimationViewController {
+    
+    private func setLabelsTitles() {
+        for index in 0..<settingsLabels.count {
+            switch index {
+                case 0: settingsLabels[index].text = "\(animationSettings.preset)"
+                case 1: settingsLabels[index].text = "\(animationSettings.curve)"
+                case 2: settingsLabels[index].text = "\(String(format: "%.2f", animationSettings.force))"
+                case 3: settingsLabels[index].text = "\(String(format: "%.2f", animationSettings.duration))"
+                default: settingsLabels[index].text = "\(String(format: "%.2f", animationSettings.delay))"
+            }
+        }
+    }
+    
 }
